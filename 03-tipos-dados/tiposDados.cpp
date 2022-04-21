c6655439999af236f7afedb13ee499e33fa3479f
Bruno Andreo <brunolino2026@gmail.com>
2022-04-20 22:29:44 -0300

    Aprendendo novos conceitos de C++

---
+++ 03-tipos-dados/tiposDados.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 2
+    double y = 3.1
+    int resultado_int;
+    double resultado_double;
+
+    resultado_int = x + y;
+    resultado_double = x + y;
+
+    cout << "Int: " << resultado_int << endl;
+    cout << "Double: " << resultado_double << endl;
+}
---
+++ 04-iostream/exIostream.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int x(5);
+
+    std::cout << "\nValor da variavel x:" << x << std::endl;
+
+    std::cout << "\nDigite um numero:";
+
+    int y;
+    std::cin >> y;
+
+    std::cout << "\nVoce digitou " << y << "\n\n";
+}
